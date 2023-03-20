0087b1161f4703f6a01d50ba848228a9a57c0ee5
pivinx1 <63850629+pivinx1@users.noreply.github.com>
2023-03-20 08:27:24 +0000

    Add Expo network component

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import * as Network from 'expo-network';
 import { Alert, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let actualIPAddr;
+  let devIPAddr = Network.getIpAddressAsync();
+  if (devIPAddr == "0.0.0.0") {
+    actualIPAddr = "i couldn't get ur ip address"
+  }
   return (
     <View style={styles.container}>
-      <Text>nice car holmes</Text>
+      <Text>you're IP address is:</Text>
+      <Text>$devIPAddr</Text>
       <Button onPress={() => Alert.alert("hello", "you probably pressed me")}/>
       <StatusBar style="auto" />
     </View>
