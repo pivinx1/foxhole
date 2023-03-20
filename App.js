093a486a10de0f706f05bb251848cd6187cbbbf5
pivinx1 <63850629+pivinx1@users.noreply.github.com>
2023-03-20 08:15:38 +0000

    Change BG color

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff0',
+    backgroundColor: 'light_gray',
     alignItems: 'center',
     justifyContent: 'center',
   },
